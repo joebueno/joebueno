a02ae7b6dd218e847acb10778de9e24334930615
joebueno <53015353+joebueno@users.noreply.github.com>
2019-07-22 11:01:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("script running!"); 
+//This is the function and definition for adding num
+function addnum(){
+        var x = parseFloat(document.getElementById('numberin1').value);
+        var y = parseFloat(document.getElementById('numberin2').value);
+        window.alert("The answer for " + x + "+" + y +" is: " + (x+y));
+}
